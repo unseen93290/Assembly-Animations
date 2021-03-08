0d2c9b3775f78dab2df48579860a5d10f7000795
Unseen <harry93290@gmail.com>
2021-03-08 08:08:13 +0100

    Nouvelle Application

---
+++ assembly_animations/android/app/src/main/kotlin/com/example/assembly_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assembly_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ assembly_animations/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
