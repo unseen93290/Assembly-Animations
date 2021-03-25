3cc366adfccef7912ddeab4735bc8e74991fb1ea
Unseen <harry93290@gmail.com>
2021-03-25 06:51:15 +0100

    Create Const.dart

---
+++ assembly_animations/lib/Const.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+var kCouleurFondEcran = Color(0xffE77F20);
+var kEcritureRecherche = TextStyle(
+  color: Colors.grey,
+  fontSize: 50,
+);
+
+var kCategorieJeux =
+    TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold);
