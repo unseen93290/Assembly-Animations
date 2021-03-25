681363593a80ce442df90505db31b03e0b07743e
Unseen <harry93290@gmail.com>
2021-03-25 06:51:45 +0100

    Create main.dart

--- assembly_animations/lib/main.dart
+++ assembly_animations/lib/main.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
+import 'Const.dart';
 
 void main() {
   runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
+  String jeuxSociete = "Jeux Societe";
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color(0xffE77F20),
+              Color(0xffffffff),
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+          ),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(30),
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Color(0xffFFFFFF),
+                    filled: true,
+                    hintText: "Recherche",
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[400],
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(jeuxSociete, style: kCategorieJeux),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(children: [
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(100),
+                      ),
+                    ),
+                    width: 200,
+                    height: 200,
+                    child: Image(
+                      image: AssetImage("Assets/Images/dooble.jpg"),
+                    ),
+                  ),
+                ),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
