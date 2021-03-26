186ed2c51a1dbf54cf85b6d80dac30af93280743
Unseen <harry93290@gmail.com>
2021-03-26 16:18:37 +0100

    [MODIFICATED] Add imageactivites a variable for browse a list

--- assembly_animations/lib/CustomWidgets/imageActivites.dart
+++ assembly_animations/lib/CustomWidgets/imageActivites.dart
@@ -1,20 +1,43 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 
 class ImagesActivites extends StatelessWidget {
-  String image;
-  ImagesActivites({this.image});
+  var image;
+  var nom;
+  var nombreJoueurs;
+  var but;
+  var materiel;
+  var deroulement;
+  Function onTap;
+
+  ImagesActivites(
+      {this.image,
+      this.onTap,
+      this.nom,
+      this.but,
+      this.deroulement,
+      this.materiel,
+      this.nombreJoueurs});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 10),
-      alignment: Alignment.topLeft,
-      width: 200,
-      height: 200,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Image(
-          image: AssetImage(image),
+    return InkWell(
+      child: Container(
+        margin: EdgeInsets.only(left: 10),
+        alignment: Alignment.topLeft,
+        width: 200,
+        height: 200,
+        child: InkWell(
+          onTap: (onTap),
+          child: Card(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image(
+                image: AssetImage(image),
+              ),
+            ),
+          ),
         ),
       ),
     );
