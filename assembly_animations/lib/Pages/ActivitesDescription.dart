d013b36ec7c7b18821f669e7e1904e6f218e4873
Unseen <harry93290@gmail.com>
2021-03-27 19:08:25 +0100

    [UPDATE] Begin Create design Activite Description

--- assembly_animations/lib/Pages/ActivitesDescription.dart
+++ assembly_animations/lib/Pages/ActivitesDescription.dart
@@ -2,10 +2,27 @@ import 'package:flutter/material.dart';
 import '../Tools/ListImageActivites.dart';
 
 class ActivitesDescription extends StatelessWidget {
+  String image = "Assets/Images/Loveletter.jpg";
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Test"),
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage(image),
+              ),
+              Container(
+                child: Card(
+                  child: Text("Nom"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
