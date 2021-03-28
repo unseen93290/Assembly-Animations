9b92a794efe7d3a42cf9552e4768bb92913a3e3b
Unseen <harry93290@gmail.com>
2021-03-28 19:09:57 +0200

    [UPDATE] Modificated way for main dart

--- assembly_animations/lib/main.dart
+++ assembly_animations/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:assembly_animations/Tools/Const.dart';
-import 'CustomWidgets/CategoriesJeux.dart';
-import 'CustomWidgets/imageActivites.dart';
-import 'package:assembly_animations/Tools/ListImageActivites.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'Pages/PageAccueil.dart';
+import 'Pages/GeneraterPage.dart';
 
 void main() {
   runApp(Myapp());
@@ -14,82 +11,7 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        bottomNavigationBar: CurvedNavigationBar(
-          items: <Widget>[
-            Icon(Icons.search, size: 20, color: Colors.black),
-            Icon(Icons.search, size: 20, color: Colors.black),
-            Icon(Icons.search, size: 20, color: Colors.black),
-            Icon(Icons.search, size: 20, color: Colors.black),
-          ],
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color(0xffE77F20),
-              Color(0xffffffff),
-            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-          ),
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(30),
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: Color(0xffFFFFFF),
-                    filled: true,
-                    hintText: "Recherche",
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 20,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey[400],
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 50,
-                height: 50,
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    CategoriesJeux(typeJeux: "Jeux Sociéte"),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      height: 200,
-                      width: 500,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemCount: activites.length,
-                        itemBuilder: (context, index) {
-                          return ImagesActivites(
-                            onTap: activites[index].onTap,
-                            image: activites[index].image,
-                            nom: activites[index].nom,
-                            nombreJoueurs: activites[index].nombreJoueurs,
-                            but: activites[index].but,
-                            materiel: activites[index].materiel,
-                            deroulement: activites[index].deroulement,
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: GeneraterPage(),
     );
   }
 }
