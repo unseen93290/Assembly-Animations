46a94b1d97b23273c19ab9c2881a246dde0c5a2e
Unseen <harry93290@gmail.com>
2021-04-02 16:28:03 +0200

    [UPDATE] Modificated image and add new widget derouler activiter

--- assembly_animations/lib/Pages/ActivitesDescription.dart
+++ assembly_animations/lib/Pages/ActivitesDescription.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+
 import '../Tools/ListImageActivites.dart';
+import '../Tools/Const.dart';
+import '../CustomWidgets/CardActivitesDescription.dart';
+import '../CustomWidgets/DeroulerActivite.dart';
 
 class ActivitesDescription extends StatelessWidget {
-  String image = "Assets/Images/dooble.jpg";
+  final String image;
   ActivitesDescription({this.image});
   @override
   Widget build(BuildContext context) {
@@ -16,66 +20,53 @@ class ActivitesDescription extends StatelessWidget {
               height: double.infinity,
               child: Image(
                 fit: BoxFit.cover,
-                image: AssetImage("Assets/Images/dooble.jpg"),
+                image: AssetImage(image),
               ),
             ),
           ),
           ListView(
             children: [
-              SizedBox(
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20),
-                height: 70,
-                width: double.infinity,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                        color: Colors.black, style: BorderStyle.solid),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                  ),
-                  color: Colors.white70,
-                  child: Text("Nom"),
+              ClipRRect(
+                child: Image(
+                  height: 150,
+                  width: 150,
+                  image: AssetImage("Assets/Images/Dooblelogo.png"),
                 ),
+              ),
+              DeroulerActivite(
+                text: "NOM",
+              ),
+              CardActivitesDescription(
+                textCardActivitesDescription: "Dooble",
               ),
               SizedBox(
                 height: 20,
               ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20),
-                height: 70,
-                width: double.infinity,
-                child: Card(
-                  shape: RoundedRectangleBorder(),
-                  color: Colors.green,
-                ),
+              DeroulerActivite(
+                text: "NOMBRES JOUEURS",
+              ),
+              CardActivitesDescription(
+                textCardActivitesDescription: " 2 a 8 Joueurs",
               ),
               SizedBox(
                 height: 20,
               ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20),
-                height: 70,
-                width: double.infinity,
-                child: Card(
-                  color: Colors.green,
-                ),
+              DeroulerActivite(
+                text: "BUT",
+              ),
+              CardActivitesDescription(
+                textCardActivitesDescription:
+                    "Quel que ce soit le jeu, il faut toujours etre le plus rapide a repérer le symbole identique , entre deux cartes, le nommer à haute voix , puis prendre la carte  la poser ou la defausser selon  les regles du mini-jeu auquel vous etes en train de jouer",
               ),
               SizedBox(
                 height: 20,
               ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20),
-                height: 270,
-                width: double.infinity,
-                child: Card(
-                  color: Colors.green,
-                ),
+              DeroulerActivite(
+                text: "DEROULEMENT",
               ),
+              CardActivitesDescription(
+                  textCardActivitesDescription:
+                      "Au top départ les joueurs retournent leur carte.Les joueurs doivent alors trouver le symbole identique entre leur carte et celle posée au centre de la table (sur la pile).Dès qu’un joueur trouve un symbole identiqueil le nomme,s’empare de la carte concernéeet la pose devant lui sur sa carte.En prenant cette carte, une nouvelle carte centrale est ainsi révélée.La partie continue jusqu’à ce que toutes les cartes de la pioche aient été récupérées"),
             ],
           ),
         ],
