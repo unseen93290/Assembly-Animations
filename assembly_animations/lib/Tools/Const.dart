f62f02b437a9d421ad55e3a36c6c0ad8e3fda7f6
Unseen <harry93290@gmail.com>
2021-04-02 16:32:09 +0200

    [ADD] New Const

--- assembly_animations/lib/Tools/Const.dart
+++ assembly_animations/lib/Tools/Const.dart
@@ -1,5 +1,7 @@
+import 'package:assembly_animations/Pages/ActivitesDescription.dart';
 import 'package:flutter/material.dart';
 
+// Page d'accueil
 var kCouleurFondEcran = Color(0xffE77F20);
 var kEcritureRecherche = TextStyle(
   color: Colors.grey,
@@ -8,3 +10,14 @@ var kEcritureRecherche = TextStyle(
 
 var kCategorieJeux =
     TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold);
+
+// ActivitesDescription
+var kStyleEcriture =
+    TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20);
+
+// Dooble
+
+String knom = "Dooble";
+String knombreJoueurs = "2 à 8 Joueurs";
+String kbut =
+    "Quel que ce soit le jeu, il faut toujours etre le plus rapide a repérer le symbole identique , entre deux cartes, le nommer à haute voix , puis prendre la carte  la poser ou la defausser selon  les regles du mini-jeu auquel vous etes en train de jouer";
