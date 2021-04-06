95561fc47f75bd0c691051d020c537b18fb7d2e3
Unseen <harry93290@gmail.com>
2021-04-06 20:25:07 +0200

    [UPDATE] add an update widget fort list activitis

--- assembly_animations/lib/Pages/PageAccueil.dart
+++ assembly_animations/lib/Pages/PageAccueil.dart
@@ -4,74 +4,71 @@ import 'package:assembly_animations/Tools/Const.dart';
 import '../CustomWidgets/CategoriesJeux.dart';
 import '../CustomWidgets/imageActivites.dart';
 import 'package:assembly_animations/Tools/ListImageActivites.dart';
+import '../CustomWidgets/ListJeuxSociete.dart';
 
 class PageAcceuil extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color(0xffE77F20),
-          Color(0xffffffff),
-        ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-      ),
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(40),
-            child: TextField(
-              decoration: InputDecoration(
-                fillColor: Color(0xffFFFFFF),
-                filled: true,
-                hintText: "Recherche",
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey[400],
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-              ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Container(
+            height: 1300,
+            width: 1300,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color(0xffE77F20),
+                Color(0xffffffff),
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
             ),
-          ),
-          SizedBox(
-            width: 50,
-            height: 50,
-          ),
-          Expanded(
             child: Column(
               children: [
-                CategoriesJeux(typeJeux: "Jeux Sociéte"),
-                SizedBox(
-                  height: 15,
-                ),
                 Container(
-                  height: 200,
-                  width: 500,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    itemCount: activites.length,
-                    itemBuilder: (context, index) {
-                      return ImagesActivites(
-                        onTap: activites[index].onTap,
-                        image: activites[index].image,
-                        nom: activites[index].nom,
-                        nombreJoueurs: activites[index].nombreJoueurs,
-                        but: activites[index].but,
-                        materiel: activites[index].materiel,
-                        deroulement: activites[index].deroulement,
-                      );
-                    },
+                  margin: EdgeInsets.all(40),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Color(0xffFFFFFF),
+                      filled: true,
+                      hintText: "Recherche",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey[400],
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 50,
+                  height: 50,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      CategoriesJeux(typeJeux: "Jeux Sociéte"),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      ListJeuxSociete(),
+                      CategoriesJeux(typeJeux: "Jeux Collectif"),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      ListJeuxSociete(),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
