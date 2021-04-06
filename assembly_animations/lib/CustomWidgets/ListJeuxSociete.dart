be11f44d6446a8b3f306e253bf15b76ca4c60f79
Unseen <harry93290@gmail.com>
2021-04-06 20:18:28 +0200

    [UPDATE] Update name picture love letter logo

---
+++ assembly_animations/lib/CustomWidgets/ListJeuxSociete.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../Tools/ListImageActivites.dart';
+import '../CustomWidgets/imageActivites.dart';
+
+class ListJeux extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 500,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: activites.length,
+        itemBuilder: (context, index) {
+          return ImagesActivites(
+            imageLogo: activites[index].imageLogo,
+            onTap: activites[index].onTap,
+            image: activites[index].image,
+            nom: activites[index].nom,
+            nombreJoueurs: activites[index].nombreJoueurs,
+            but: activites[index].but,
+            materiel: activites[index].materiel,
+            deroulement: activites[index].deroulement,
+            age: activites[index].age,
+          );
+        },
+      ),
+    );
+  }
+}
