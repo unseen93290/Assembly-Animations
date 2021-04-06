78a7ed6a5eb348e7b4987206e2a51167d17aa211
Unseen <harry93290@gmail.com>
2021-04-06 20:27:25 +0200

    [ADD & UPDATE] add new variable age for list and i create new list for jeux collectif

--- assembly_animations/lib/Tools/ListImageActivites.dart
+++ assembly_animations/lib/Tools/ListImageActivites.dart
@@ -4,51 +4,40 @@ import 'package:flutter/material.dart';
 
 List<ImagesActivites> activites = [
   ImagesActivites(
-      nom: "Dooble",
-      nombreJoueurs: "2 a 8",
-      but: "Blablabalabl",
-      materiel: "zkfzofkzofkzok",
-      deroulement: "ifjeifjeijfeifje",
-      image: "Assets/Images/dooble.jpg",
-      onTap: () {
-        print("test");
-
-        BuildContext context;
-        Navigator.push(context,
-            MaterialPageRoute(builder: (BuildContext context) {
-          return ActivitesDescription(
-            image: "Assets/Images/dooble.jpg",
-          );
-        }));
-      }),
+    nom: "Dooble",
+    nombreJoueurs: "2 a 8 Joueurs",
+    but:
+        "Quel que soit le jeu, il faut toujours être le plus rapide à repérer le symbole identique entredeux cartes, le nommer à haute voix, puis prendre la carte, la poser ou la défausser selon les règles du mini-jeu auquel vous êtes en train déjouer",
+    materiel: "Aucun",
+    deroulement:
+        "Tous les joueurs jouent en même temps.Quelque soit la variante jouée, il faut toujours :être le plus rapide à repérer le symbole identique entre 2 cartes,le nommer à voix haute puis (selon la variante), prendre la carte, la poser ou la défausser",
+    image: "Assets/Images/dooble.jpg",
+    imageLogo: "Assets/Images/DoobleLogo.png",
+    age: "6 ans et Plus",
+  ),
   ImagesActivites(
-      nom: "love letter",
-      nombreJoueurs: "2 a 8",
-      but: "Blablabalabl",
-      materiel: "zkfzofkzofkzok",
-      deroulement: "ifjeifjeijfeifje",
-      image: "Assets/Images/Loveletter.jpg",
-      onTap: () {
-        print("test");
-        /*Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) {
-              return ActivitesDescription();
-            }));*/
-      }),
+    nom: "Love Letter",
+    nombreJoueurs:
+        "2-4 (jusqu’à 6 avec l’édition Z-Man) grâce à deux nouveaux personnages : Espionne + Chancelier. Et jusqu’à 8 avec l’édition Premium qui rajoute encore plus de personnages).",
+    but:
+        "Terminer la manche en ayant en main la carte avec le rang le plus élevé. Le joueur gagne alors un jeton cœur.",
+    materiel: "16 cartes + des jetons cœur.",
+    deroulement:
+        "Piocher 1 carte : Le premier joueur pioche une carte et l’ajoute à sa main.Défausser une carte : Il choisit une de ses cartes, la défausse en la posant devant lui, face visible et en applique les effets (voir la liste des cartes).Les cartes jouées restent devant chacun des joueurs dans l’ordre où elles ont été jouées.Une fois les effets de la cartes appliqués, c’est au joueur suivant de jouer.Si un joueur est éliminé de la manche, il défausse sa main face visible, mais ne pioche pas de nouvelle carte.",
+    image: "Assets/Images/Loveletter.jpg",
+    imageLogo: "Assets/Images/LoveLetterLogo.jpg",
+    age: "6 ans et Plus",
+  ),
   ImagesActivites(
-      nom: "Mille Borne",
-      nombreJoueurs: "2 a 8",
-      but: "Blablabalabl",
-      materiel: "zkfzofkzofkzok",
-      deroulement: "ifjeifjeijfeifje",
-      image: "Assets/Images/MilleBornes.png",
-      onTap: () {
-        print("test");
-        /*Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) {
-              return ActivitesDescription();
-            }));*/
-      }),
+    nom: "Mille Borne",
+    nombreJoueurs: "2 a 8",
+    but: "Blablabalabl",
+    materiel: "zkfzofkzofkzok",
+    deroulement: "ifjeifjeijfeifje",
+    image: "Assets/Images/MilleBornes.png",
+    imageLogo: "Assets/Images/DoobleLogo.png",
+    age: "6 ans et Plus",
+  ),
   ImagesActivites(
       nom: "Dooble",
       nombreJoueurs: "2 a 8",
@@ -56,13 +45,7 @@ List<ImagesActivites> activites = [
       materiel: "zkfzofkzofkzok",
       deroulement: "ifjeifjeijfeifje",
       image: "Assets/Images/dooble.jpg",
-      onTap: () {
-        print("test");
-        /*Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) {
-              return ActivitesDescription();
-            }));*/
-      }),
+      imageLogo: "Assets/Images/DoobleLogo.png"),
   ImagesActivites(
       nom: "Dooble",
       nombreJoueurs: "2 a 8",
@@ -70,13 +53,7 @@ List<ImagesActivites> activites = [
       materiel: "zkfzofkzofkzok",
       deroulement: "ifjeifjeijfeifje",
       image: "Assets/Images/dooble.jpg",
-      onTap: () {
-        print("test");
-        /*Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) {
-              return ActivitesDescription();
-            }));*/
-      }),
+      imageLogo: "Assets/Images/DoobleLogo.png"),
   ImagesActivites(
       nom: "Dooble",
       nombreJoueurs: "2 a 8",
@@ -84,13 +61,7 @@ List<ImagesActivites> activites = [
       materiel: "zkfzofkzofkzok",
       deroulement: "ifjeifjeijfeifje",
       image: "Assets/Images/dooble.jpg",
-      onTap: () {
-        print("test");
-        /*Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) {
-              return ActivitesDescription();
-            }));*/
-      }),
+      imageLogo: "Assets/Images/DoobleLogo.png"),
   ImagesActivites(
       nom: "Dooble",
       nombreJoueurs: "2 a 8",
@@ -98,12 +69,18 @@ List<ImagesActivites> activites = [
       materiel: "zkfzofkzofkzok",
       deroulement: "ifjeifjeijfeifje",
       image: "Assets/Images/dooble.jpg",
-      onTap: () {
-        print("test");
-        /*Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) {
-              return ActivitesDescription();
-            }));*/
-      }),
+      imageLogo: "Assets/Images/DoobleLogo.png"),
 ];
-//return MaterialApp();
+
+List<ImagesActivites> jeuxCollectif = [
+  ImagesActivites(
+    nom: "Drapeau",
+    nombreJoueurs: "10 à 99",
+    but: "Blablabla",
+    materiel: "Plots , Chassuble , Poteau , Coupelles",
+    deroulement: "",
+    image: "Assets/Images/Drapeau.jpg",
+    imageLogo: "Assets/Images/DapeauLogo.jpg",
+    age: "Des 6 ans",
+  ),
+];
