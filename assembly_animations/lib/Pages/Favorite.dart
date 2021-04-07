89a3ce66cb09f318e3ab9d4340b349ec12e0074c
Unseen <harry93290@gmail.com>
2021-04-07 17:59:44 +0200

    [UPDATE] Create card for favorite game

--- assembly_animations/lib/Pages/Favorite.dart
+++ assembly_animations/lib/Pages/Favorite.dart
@@ -1,6 +1,47 @@
+import 'package:flutter/material.dart';
+
 class Favorite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: 1300,
+            width: 1300,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color(0xffE77F20),
+                Color(0xffffffff),
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+            ),
+            child: ListView(
+              children: [
+                Stack(
+                  children: [
+                    Card(
+                      margin: EdgeInsets.all(10),
+                      color: Colors.grey,
+                      child: Container(
+                        height: 100,
+                        child: Opacity(
+                          opacity: 0.4,
+                          child: Image(
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            image: AssetImage("Assets/Images/dooble.jpg"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
