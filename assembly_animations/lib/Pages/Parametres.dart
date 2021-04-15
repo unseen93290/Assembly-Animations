3619ab8a013c97a88fc3b8010deec73ec7dd488a
Unseen <harry93290@gmail.com>
2021-04-15 10:51:17 +0200

    [ADD] i add a page temporary

---
+++ assembly_animations/lib/Pages/Parametres.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Parametres extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        "En cour de Creation de la Page Parametre",
+        style: TextStyle(fontSize: 30, color: Colors.red),
+      ),
+    );
+  }
+}
