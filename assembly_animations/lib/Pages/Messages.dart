be3156f22df288eb206b5780f401fa9a5a90e3bd
Unseen <harry93290@gmail.com>
2021-04-15 10:51:06 +0200

    [ADD] i add a page temporary

--- assembly_animations/lib/Pages/Messages.dart
+++ assembly_animations/lib/Pages/Messages.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        "En cour de Creation de la page messages",
+        style: TextStyle(fontSize: 30, color: Colors.red),
+      ),
+    );
+  }
+}
