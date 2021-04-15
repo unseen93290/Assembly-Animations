eb4e73ba10a9f15d111afdd48217e575d0f18090
Unseen <harry93290@gmail.com>
2021-04-15 10:50:25 +0200

    [ADD] i add a page temporary

---
+++ assembly_animations/lib/Pages/AjoutActivites.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AjoutActivites extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        "En cour de Creation de la page ajout actvites ",
+        style: TextStyle(fontSize: 30, color: Colors.red),
+      ),
+    );
+  }
+}
