1fc1bf764f58276ff399225f86ae834356f4be4f
Unseen <harry93290@gmail.com>
2021-04-19 10:27:04 +0200

    [UPDATE] I make update for Authentification because un dont like this preceded page

--- assembly_animations/lib/Pages/Authentification.dart
+++ assembly_animations/lib/Pages/Authentification.dart
@@ -1,6 +1,7 @@
+import 'package:assembly_animations/CustomWidgets/RegisterButton.dart';
 import 'package:assembly_animations/Pages/GeneraterPage.dart';
 import 'package:assembly_animations/Tools/Const.dart';
-import 'package:assembly_animations/Tools/Loading.dart';
+
 import 'package:flutter/material.dart';
 
 class Authentification extends StatefulWidget {
@@ -9,144 +10,75 @@ class Authentification extends StatefulWidget {
 }
 
 class _AuthentificationState extends State<Authentification> {
-  bool showSignIn = true;
-  String error = "";
-  bool loading = false;
-  final formKey = GlobalKey<FormState>(); //
-  final userController =
-      TextEditingController(); // Controle si le texte est bon , si le champ n'est pas vide ect ...
-  final motDePasseController = TextEditingController();
-  @override
-  void dispose() {
-    // Le dispose permet de vider le cache memoire pour initialiser les controleur
-    userController.dispose();
-    motDePasseController.dispose();
-    super.dispose();
-  }
-
-  void accesSwitch() {
-    setState(() {
-      userController.text = "";
-      motDePasseController.text = "";
-      showSignIn = !showSignIn;
-    });
-  }
-
   Widget build(BuildContext context) {
-    return loading
-        ? Loading()
-        : MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: SafeArea(
-              child: Scaffold(
-                body: SingleChildScrollView(
-                  child: Column(children: [
-                    Container(
-                      height: 1300,
-                      width: 1300,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [
-                              Color(0xffE77F20),
-                              Color(0xffffffff),
-                            ],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter),
-                      ),
-                      child: Center(
-                        child: Form(
-                          key: formKey,
-                          child: ListView(
-                            children: [
-                              Container(
-                                child: TextButton.icon(
-                                  onPressed: () {
-                                    accesSwitch();
-                                  },
-                                  label: Text(
-                                    showSignIn
-                                        ? "Inscrivez vous "
-                                        : "Connectez vous ",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  icon: Icon(
-                                    Icons.exit_to_app,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              ClipRRect(
-                                child: Image(
-                                  height: 200,
-                                  width: 200,
-                                  image: AssetImage(
-                                    "Assets/Images/dooble.jpg",
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: TextFormField(
-                                  decoration: textInputDecoration.copyWith(
-                                      hintText: "Email/Pseudo"),
-                                  controller: userController,
-                                  validator: (value) {
-                                    print(value);
-                                    if (value == value.isEmpty) {
-                                      // Si la valeur est vide alor message d'erreur
-
-                                      return "Entrer votre email ou pseudo";
-                                    }
-                                    return null;
-                                  },
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: TextFormField(
-                                    obscureText: true,
-                                    decoration: textInputDecoration.copyWith(
-                                        hintText: "Mot de passe"),
-                                    controller: motDePasseController,
-                                    validator: (value) => value.length < 6
-                                        ? "Veuillez rentrer 6 caracteres minimum"
-                                        : null),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Center(
-                                // ignore: deprecated_member_use
-                                child: FlatButton(
-                                  disabledColor: Colors.grey,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(30)),
-                                  color: Colors.white,
-                                  onPressed: () {
-                                    print("Appuyer");
-                                    Navigator.push(context,
-                                        MaterialPageRoute<void>(
-                                            builder: (BuildContext context) {
-                                      return GeneraterPage();
-                                    }));
-                                  },
-                                  child: Text(showSignIn
-                                      ? "Conectez vous "
-                                      : "Enregistrez vous"),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ]),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: ListView(
+            children: [
+              Container(
+                alignment: Alignment.bottomLeft,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
               ),
-            ),
-          );
+              Hero(
+                tag: "DoobleLogo",
+                child: Container(
+                  height: 200,
+                  child: Image.asset(
+                    "Assets/Images/DoobleLogo.png",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 50,
+                margin: EdgeInsets.only(bottom: 17, left: 20, right: 20),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  decoration: textInputDecoration.copyWith(
+                      hintText: "Entrer votre email"),
+                ),
+              ),
+              Container(
+                height: 50,
+                margin: EdgeInsets.only(bottom: 10, left: 20, right: 20),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  decoration: textInputDecoration.copyWith(
+                      hintText: "Entrer votre mot de passe"),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RegisterButton(
+                text: "Connecter",
+                onPressed: () {
+                  print("Appuyer");
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
