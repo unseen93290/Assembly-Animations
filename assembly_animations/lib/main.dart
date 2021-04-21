2eaadb3772021f48cbc4124af11f11de3bc6ddf9
Unseen <harry93290@gmail.com>
2021-04-21 10:42:51 +0200

    [UPGRADE] Add initialize firebase

--- assembly_animations/lib/main.dart
+++ assembly_animations/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:assembly_animations/Pages/Authentification.dart';
 import 'package:assembly_animations/Pages/PageAccueil.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'Pages/Home.dart';
 import 'Pages/GeneraterPage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Myapp());
 }
 
