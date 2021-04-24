47cfb6237814f274b26f585a470e1196bca430f1
Unseen <harry93290@gmail.com>
2021-04-24 19:41:08 +0200

    [UPGRADE] Change inputdecoration

--- assembly_animations/lib/Tools/Const.dart
+++ assembly_animations/lib/Tools/Const.dart
@@ -30,6 +30,12 @@ const kTextInputDecoration = InputDecoration(
       Radius.circular(30),
     ),
   ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black, width: 2),
+    borderRadius: BorderRadius.all(
+      Radius.circular(30),
+    ),
+  ),
   errorBorder: OutlineInputBorder(
     borderSide: BorderSide(
       color: Colors.red,
