082102d081f52e6527b6bb057f4d4d10679f8b24
Unseen <harry93290@gmail.com>
2021-04-27 18:49:37 +0200

    [UPGRADE]i create page message whith contact

--- assembly_animations/lib/Pages/Messages.dart
+++ assembly_animations/lib/Pages/Messages.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import '../Tools/ListContact.dart';
+import '../CustomWidgets/Contact.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //TODO Construire la page message voir le cour d'angela
-    return Container(
-      alignment: Alignment.center,
-      child: Text(
-        "En cour de Creation de la page messages",
-        style: TextStyle(fontSize: 30, color: Colors.red),
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Theme.of(context).primaryColor,
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: contact.length,
+          itemBuilder: (context, index) {
+            return Contact(
+              image: contact[index].image,
+              name: contact[index].name,
+              lastMessage: contact[index].lastMessage,
+            );
+          },
+        ),
       ),
     );
   }
