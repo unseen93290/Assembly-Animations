7cb9b37ef0d17545e6ce43bb3c22a74fe92c4a4b
Unseen <harry93290@gmail.com>
2021-04-27 18:50:22 +0200

    [ADD]i add new list for contact

---
+++ assembly_animations/lib/Tools/ListContact.dart
@@ -0,0 +1,19 @@
+import '../CustomWidgets/Contact.dart';
+
+List<Contact> contact = [
+  Contact(
+    image: "Assets/Images/DoobleLogo.png",
+    name: "Bouzid Nabil",
+    lastMessage: "Salut sa va ?",
+  ),
+  Contact(
+    image: "Assets/Images/DoobleLogo.png",
+    name: "Bouzid Nabil",
+    lastMessage: "Salut sa va ?",
+  ),
+  Contact(
+    image: "Assets/Images/DoobleLogo.png",
+    name: "Bouzid Nabil",
+    lastMessage: "Salut sa va ?",
+  ),
+];
