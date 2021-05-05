b009a7ac1a62922234756dc09bc41501819d8026
Unseen <harry93290@gmail.com>
2021-05-05 17:41:09 +0200

    [ADD] TODO / Padding ect...

--- assembly_animations/lib/Pages/Favorite.dart
+++ assembly_animations/lib/Pages/Favorite.dart
@@ -1,5 +1,7 @@
 import 'package:assembly_animations/CustomWidgets/CardFavorite.dart';
+import 'package:assembly_animations/Pages/ActivitesDescription.dart';
 import 'package:flutter/material.dart';
+import '';
 
 class Favorite extends StatefulWidget {
   @override
@@ -13,17 +15,32 @@ class _FavoriteState extends State<Favorite> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
-        body: ListView(
-          children: [
-            CardFavorite(
-              image: "Assets/Images/dooble.jpg",
-              nom: "Dooble",
-            ),
-            CardFavorite(
-              image: "Assets/Images/Drapeau.jpg",
-              nom: "Drapeau",
-            )
-          ],
+        body: SafeArea(
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+            children: [
+              //TODO Voir widget disimille pour suprimer une chose d'un balayage de doigt
+
+              CardFavorite(
+                image: "Assets/Images/DoobleLogo.png",
+                nom: "Dooble",
+              ),
+
+              CardFavorite(
+                image: "Assets/Images/Drapeau.jpg",
+                nom: "Drapeau",
+              ),
+              CardFavorite(
+                image: "Assets/Images/DrapeauLogo.png",
+                nom: "Drapeau",
+              ),
+              CardFavorite(
+                image: "Assets/Images/Drapeau.jpg",
+                nom: "Drapeau",
+              ),
+            ],
+          ),
         ),
       ),
     );
