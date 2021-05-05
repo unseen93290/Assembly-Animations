f4fedae0b4ec974578b607c32309fb81a3348c4f
Unseen <harry93290@gmail.com>
2021-05-05 17:38:14 +0200

    [ADD] I add iconButton for description game

--- assembly_animations/lib/CustomWidgets/CardFavorite.dart
+++ assembly_animations/lib/CustomWidgets/CardFavorite.dart
@@ -3,10 +3,9 @@ import 'package:provider/provider.dart';
 
 class CardFavorite extends StatefulWidget {
   String image;
-  bool favorite = true;
-  String nom;
-  CardFavorite({this.image, this.favorite, this.nom});
 
+  String nom;
+  CardFavorite({this.image, this.nom});
   @override
   _CardFavoriteState createState() => _CardFavoriteState();
 }
@@ -15,15 +14,16 @@ class _CardFavoriteState extends State<CardFavorite> {
   bool favorite = true;
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Opacity(
-          opacity: 0.6,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
           // IMPORTANTT !!!!! Si jamais tu veux cree une card ou une image et que l'arondissement des coter ce fais mais que le container reste rectangle voila comment faire
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          Container(
             height: 100,
+            width: 400,
             decoration: BoxDecoration(
+                color: Colors.white,
                 image: DecorationImage(
                   fit: BoxFit.cover,
                   image: AssetImage(
@@ -32,34 +32,41 @@ class _CardFavoriteState extends State<CardFavorite> {
                 ),
                 borderRadius: BorderRadius.circular(20)),
           ),
-        ),
-        Positioned(
-          right: 20,
-          bottom: 50,
-          child: IconButton(
-            iconSize: 40,
-            color: Colors.black,
-            icon: favorite ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
-            onPressed: () {
-              setState(() {
-                //TODO Demander a mohamed pourquoi quand je met savorite dans la premiere parti du stateful et que j'utilise widget.favorite je me retrouve avec un nul alor que sa fonctionne pour l'image
-                if (favorite == true) {
-                  favorite = false;
-                } else {
-                  favorite = true;
-                }
-              });
-            },
+
+          Positioned(
+            child: IconButton(
+              iconSize: 40,
+              color: Colors.black,
+              icon:
+                  favorite ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
+              onPressed: () {
+                setState(() {
+                  //TODO Demander a mohamed pourquoi quand je met savorite dans la premiere parti du stateful et que j'utilise widget.favorite je me retrouve avec un nul alor que sa fonctionne pour l'image
+                  if (favorite == true) {
+                    favorite = false;
+                  } else {
+                    favorite = true;
+                  }
+                });
+              },
+            ),
           ),
-        ),
-        Positioned(
-          child: Text(
-            widget.nom,
-            style: TextStyle(
-                fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ],
+          Positioned(
+              right: 1,
+              top: 60,
+              child: IconButton(
+                icon: Icon(
+                  Icons.bar_chart,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                onPressed: () {
+                  //TODO Ne pas oublier de retirer le print
+                  print("Appuyer");
+                },
+              )),
+        ],
+      ),
     );
   }
 }
