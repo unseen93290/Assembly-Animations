9c030006d9081434dc84099f75e1468979b67668
Unseen <harry93290@gmail.com>
2021-05-24 19:53:09 +0200

    [UPGRADE] I add a PageRouteBuilder for style change page

--- assembly_animations/lib/CustomWidgets/imageActivites.dart
+++ assembly_animations/lib/CustomWidgets/imageActivites.dart
@@ -29,19 +29,40 @@ class ImagesActivites extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        //TODO Cree une animation grace a PageRouteBuilder si oublie voir david youtube
-        Navigator.push(context,
-            MaterialPageRoute(builder: (BuildContext context) {
-          return ActivitesDescription(
-              nombreJoueurs: nombreJoueurs,
-              image: image,
-              nom: nom,
-              but: but,
-              deroulement: deroulement,
-              materiel: materiel,
-              imageLogo: imageLogo,
-              age: age);
-        }));
+        Navigator.push(
+            context,
+            PageRouteBuilder(
+                //Permet de cree une animation de page
+                transitionDuration: Duration(milliseconds: 400),
+                transitionsBuilder: (BuildContext context,
+                    Animation<double> animation,
+                    Animation<double> secAnimation,
+                    Widget child) {
+                  var begin = Offset(0.0, 1.0);
+                  //les offset comprene les x et les y les y representer par 1 indique qu'on par du bas car 0x est la parti du haut
+                  var end = Offset.zero;
+                  var tween = Tween(
+                      begin: begin,
+                      end:
+                          end); //tween permet de mettre deux chose en meme temps niveau animation il ne faut pas oublier le chain pour l'animation je l'utilise pas car j'ai duration la haut
+                  return SlideTransition(
+                    // l'animation par du bas vers le haut
+                    position: animation.drive(tween),
+                    child: child,
+                  );
+                },
+                pageBuilder: (BuildContext context, Animation<double> animation,
+                    Animation<double> secAnimation) {
+                  return ActivitesDescription(
+                      nombreJoueurs: nombreJoueurs,
+                      image: image,
+                      nom: nom,
+                      but: but,
+                      deroulement: deroulement,
+                      materiel: materiel,
+                      imageLogo: imageLogo,
+                      age: age);
+                }));
       },
       child: Container(
         margin: EdgeInsets.all(12),
